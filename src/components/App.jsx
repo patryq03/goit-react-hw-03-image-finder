d7a7deb03def90e27f5e31e9394d80e8ac5d178d
patryq03 <138616976+patryq03@users.noreply.github.com>
2024-01-31 16:48:33 +0100

    feat: change src folder

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,25 +1,55 @@
-import { Component } from "react";
-import Searchbar from "./Searchbar/Searchbar"; 
+import { Component } from 'react';
+import Searchbar from './Searchbar/Searchbar';
 import ImageGallery from './ImageGallery/ImageGallery';
 //import Loader from "./Loader/Loader";
 //import Button from "./Button/Button";
 //import Modal from "./Modal/Modal";
 import axios from 'axios';
+import Notiflix from 'notiflix';
 
-axios.defaults.baseURL =
-  'https://pixabay.com/api/?q=cat&page=1&key=your_key&image_type=photo&orientation=horizontal&per_';
+axios.defaults.baseURL = 'https://pixabay.com/api/';
+const apiKey = '41316122-8be1516af8b9dd89b7470b6b1';
+const perPage = 12;
 
-class App extends Component{
-
+class App extends Component {
   state = {
-    id: '',
-    activePage: 1,
-    numberOfPages: 0,
+    images: [],
     isLoading: false,
     error: null,
-    webformatURL: '',
-    largeImageURL: '',
+    activePage: 1,
+    searchQuery: '',
+    totalImages: 0,
+    modalIsOpen: false,
+    largeImageUrl: '',
   };
+
+  searchImages = query => {
+    if (query.trim() === '') {
+      Notiflix.Notify.info('Sorry, please provide a search word');
+      return;
+    }
+    this.setState({
+      images: [],
+      activePage: 1,
+      searchQuery: `${Date.now()}/${query}`,
+    });
+  };
+
+  sloadMoreImages = (prev) =>{
+    this.setState({activePage: prev.activePage + 1});
+  };
+  
+  componentDidUpdate = (prevProps, prevState) => {
+    const prevQuery = prevState.searchQuery;
+    const nextQuery = this.state.searchQuery;
+    const prevPage = prevState.activePage;
+    const nexPage = this.state.activePage;
+
+    if (prevQuery !== nextQuery || prevPage !== nexPage) {
+      this.getImages();
+    }
+  };
+
   getCharacters = async () => {
     this.setState({
       isLoading: true,
@@ -27,12 +57,10 @@ class App extends Component{
 
     const { activePage } = this.state;
     try {
-      //const { data } = await axios(`page=${activePage}`);
-      console.log(activePage);
-      
-      this.setState({
+      const { data } = await axios(`?page=${activePage}`);
+      console.log(data);
 
-      })
+      //this.setState({});
     } catch (error) {
       this.setState({
         error,
@@ -46,15 +74,13 @@ class App extends Component{
   async componentDidMount() {
     await this.getCharacters();
   }
-  render(){
-    return(
+  render() {
+    return (
       <div>
         <Searchbar onSubmit={this.searchImages} />
-        <ImageGallery 
-        
-        />
+        <ImageGallery />
       </div>
-    )
+    );
   }
 }
 
